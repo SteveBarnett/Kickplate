52904a73d1fda03b43ad947a95f54c2e2b66909f
Steve Barnett <steve@naga.co.za>
2018-02-06 11:19:15 +0200

    Update header.php

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
 <link rel="apple-touch-icon" href="apple-touch-icon.png">
